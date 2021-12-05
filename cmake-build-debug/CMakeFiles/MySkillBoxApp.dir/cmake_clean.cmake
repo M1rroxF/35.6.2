39f75c0b6a63c6ab644b95cce664a26a79e63724
PuShO4Ek <Stepan.Ulyanov.2007@mail.ru>
2021-12-05 16:43:51 +0400

    projectFiles

---
+++ cmake-build-debug/CMakeFiles/MySkillBoxApp.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/MySkillBoxApp.dir/main.cpp.obj"
+  "MySkillBoxApp.exe"
+  "MySkillBoxApp.exe.manifest"
+  "MySkillBoxApp.pdb"
+  "libMySkillBoxApp.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/MySkillBoxApp.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
