cd953ecf41fc8b49a064e69a5f1bb7c27c58acf6
PuShO4Ek <Stepan.Ulyanov.2007@mail.ru>
2021-12-05 19:11:20 +0400

    33.5.2 finish #1

--- main.cpp
+++ main.cpp
@@ -1,79 +1,39 @@
 #include <iostream>
 #include <string>
-#include <map>
-#include <exception>
+#include <cstdlib>
+#include <ctime>
 //#include "boost/asio.hpp"
 
 using namespace std;
 //using namespace boost::asio;
 
-void check(const map<string, int>& base, const map<string, int>& basket, const string& move, const string& name, int& count) {
-    auto itBase = base.find(name);
-    auto itBasket = basket.find(name);
-
-    if (itBasket == basket.cend()) throw name;
-    if (count > (move == "add" ? itBase->second : itBasket->second) || count < 1) throw count;
+int random(const int& min, const int& max) {
+    return (rand() % (max - min + 1)) + min;
 }
 
-// ########################
+void generate(const int& n) {
+    srand(time(nullptr));
+    int n1 = random(1, 9);
+    int n2 = random(1,9);
+    while (n1 == n2) n2 = random(1, 9);
+
+    if (n == n1) throw (string) "you win";
+    if (n == n2) throw (string) "you lose";
+}
+
 int main() {
-    map<string, int> dataBase;
-    map<string, int> basket;
-
+    int attempts = 0;
     do {
+        int n;
+        cout << ">>"; cin >> n;
         try {
-            string name;
-            int count;
-
-            cout << "Enter product or exit: ";
-
-            cin >> name;
-            if (name == "exit") break;
-
-            cin >> count;
-            if (count < 1) throw count;
-
-            dataBase.insert(pair<string, int>(name, count));
-            basket.insert(pair<string, int> (name, 0));
+            attempts+=1;
+            generate(n);
         }
-        catch (int x) {
-            cout << "Error number: " << x;
+        catch (const string& x) {
+            cout << x << endl << "attempts: " << attempts << endl;
+            return 0;
         }
-    } while (true);
-
-
-
-    do {
-        string move;
-
-        cout << ">>";
-        cin >> move;
-
-        if (move == "add" || move == "remove") {
-            try {
-                string name;
-                int count;
-
-                cin >> name >> count;
-                auto itBase = dataBase.find(name);
-                auto itBasket = basket.find(name);
-
-                check(dataBase, basket, move, name, count);
-
-                (move == "add" ? itBase->second : itBasket->second) -= count;
-                (move == "add" ? itBasket->second : itBase->second) += count;
-                cout << "yes" << endl;
-            }
-            catch (const string &x) {
-                cout << "Error name: " << x << endl;
-            }
-            catch (int x) {
-                cout << "Error count: " << x << endl;
-            }
-        }
-        else if (move == "list") {
-            for (auto& it : basket) cout << it.first << " - " << it.second << endl;
-        }
-        else if (move == "exit") break;
+        cout << "nothing" << endl;
     } while (true);
 }
