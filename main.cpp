3df34a1824603b1213875fa0f2910d82b9b308ed
PuShO4Ek <Stepan.Ulyanov.2007@mail.ru>
2021-12-06 14:50:10 +0400

    33.5.2 finish #2

--- main.cpp
+++ main.cpp
@@ -2,6 +2,7 @@
 #include <string>
 #include <cstdlib>
 #include <ctime>
+#include <exception>
 //#include "boost/asio.hpp"
 
 using namespace std;
@@ -11,14 +12,28 @@ int random(const int& min, const int& max) {
     return (rand() % (max - min + 1)) + min;
 }
 
+class Win: public exception {
+  public:
+    char* what() const noexcept override {
+        return "You win!";
+    }
+};
+
+class Lose: public exception {
+  public:
+    char* what() const noexcept override {
+        return "You lose!";
+    }
+};
+
 void generate(const int& n) {
     srand(time(nullptr));
     int n1 = random(1, 9);
     int n2 = random(1,9);
     while (n1 == n2) n2 = random(1, 9);
 
-    if (n == n1) throw (string) "you win";
-    if (n == n2) throw (string) "you lose";
+    if (n == n1) throw Win();
+    if (n == n2) throw Lose();
 }
 
 int main() {
@@ -30,8 +45,8 @@ int main() {
             attempts+=1;
             generate(n);
         }
-        catch (const string& x) {
-            cout << x << endl << "attempts: " << attempts << endl;
+        catch (const exception& x) {
+            cout << x.what() << endl << "attempts: " << attempts << endl;
             return 0;
         }
         cout << "nothing" << endl;
