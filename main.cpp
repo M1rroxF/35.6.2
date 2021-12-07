d6dac57a0aaa39d81aa84eb89aa962deae05da72
PuShO4Ek <Stepan.Ulyanov.2007@mail.ru>
2021-12-07 18:32:50 +0400

    36.5.2 finish #1

--- main.cpp
+++ main.cpp
@@ -1,9 +1,20 @@
 #include <iostream>
+#include <unordered_set>
+#include <memory>
+#include <vector>
 
 using namespace std;
 
+auto getCount = [](vector<int> vec) {
+    unordered_set<int> uno;
+    for (auto &i : vec) uno.emplace(i);
+    vec.clear();
+    for (auto &i : uno) vec.push_back(i);
+    return make_unique<vector<int>>(vec);
+};
+
 int main() {
-    initializer_list<int> f = {1, 2, 3, 4, 5};
-    for (auto& i: f)
-        cout << i << endl;
+    vector<int> vec = {3, 3, 2, 1, 2, 1, 5};
+    unique_ptr<vector<int>> uni = getCount(vec);
+    for (auto &i : *uni) cout << i << endl;
 }
